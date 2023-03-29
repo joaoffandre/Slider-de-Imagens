1fd27b2f83b2e3e64e2699862eeffd24dd8bae4f
Joao Francisco <joaojorpas@hotmail.com>
2023-03-28 22:28:29 -0300

    New functions added to arrows

--- src/js/index.js
+++ src/js/index.js
@@ -7,21 +7,33 @@ function verificaComecoFim(item,index,array){
         const setaEsquerda = document.querySelector(".seta-esquerda")
         setaEsquerda.classList.add("seta-extrema")
     } else if (index == array.length-1){
-        const setaDireita = document.querySelector(".seta-direita")
+        const setaDireita = document.querySelectorAll(".seta-direita")[index]
         setaDireita.classList.add("seta-extrema")
     }
 }
 
 const setas = document.querySelectorAll(".seta")
+const setasEsquerda=document.querySelectorAll(".seta-esquerda")
+const setasDireita=document.querySelectorAll(".seta-direita")
 
 setas.forEach(verificaSetaValida)
 
 function verificaSetaValida(item,index,array){
     if (!item.classList.contains("seta-extrema")){
-        item.addEventListener("click",passarSlider)
+        item.addEventListener("click",passarSlide)
     }
 }
 
-function passarSlider(){
-    console.log ("teste")
+function passarSlide(evento){
+    const arrayEsquerda = Array.from(setasEsquerda)
+    const arrayDireita = Array.from(setasDireita)
+    if (arrayEsquerda.includes(evento.target)){
+        const indice = arrayEsquerda.indexOf(evento.target)
+        sliders[indice].classList.add("voltar")
+        sliders[indice].classList.remove("passar")
+    } else{
+        const indice = arrayDireita.indexOf(evento.target)
+        sliders[indice+1].classList.add("passar") 
+        sliders[indice+1].classList.remove("voltar")
+    }
 }
